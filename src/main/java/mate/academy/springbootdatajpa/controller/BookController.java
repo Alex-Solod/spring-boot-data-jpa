fcad44d6ecb47a65cad1b0f387a4ee1eef34bcd6
Oleksandr Solodukhin <78688752+Alex-Solod@users.noreply.github.com>
2026-02-18 00:00:59 +0200

    Add pagination, sorting and Swagger support to controllers
    - Implemented Pageable in BookController
    - Updated BookService to return Page<BookDto>
    - Added sorting support via Pageable
    - Integrated Swagger (springdoc-openapi) for API documentation

--- src/main/java/mate/academy/springbootdatajpa/controller/BookController.java
+++ src/main/java/mate/academy/springbootdatajpa/controller/BookController.java
@@ -1,16 +1,28 @@
 package mate.academy.springbootdatajpa.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.springbootdatajpa.dto.BookDto;
 import mate.academy.springbootdatajpa.dto.CreateBookRequestDto;
 import mate.academy.springbootdatajpa.dto.UpdateBookRequestDto;
 import mate.academy.springbootdatajpa.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@Tag(name = "Product management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
@@ -18,8 +30,9 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.getAllBooks();
+    @Operation(summary = "Get all books", description = "Get list of all available books")
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.getAllBooks(pageable);
     }
 
     @GetMapping("/{id}")
@@ -28,6 +41,7 @@ public class BookController {
     }
 
     @PostMapping
+    @Operation(summary = "Crate a new book", description = "Create a new book")
     public BookDto save(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
--- src/main/java/mate/academy/springbootdatajpa/repository/BookRepository.java
+++ src/main/java/mate/academy/springbootdatajpa/repository/BookRepository.java
@@ -1,12 +1,12 @@
 package mate.academy.springbootdatajpa.repository;
 
 import mate.academy.springbootdatajpa.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByIdAndDeletedFalse(Long id);
-    List<Book> findAllByDeletedFalse();
+    Page<Book> findAllByDeletedFalse(Pageable pageable);
 }
